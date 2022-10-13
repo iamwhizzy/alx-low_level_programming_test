6db01596cd6f099ddea1b192c5d84839c3935bcd
iamwhizzy <forlareen@gmail.com>
2022-10-13 14:39:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
 *
 */
 
-int main(void);
+int main()
 
-{puts("\"Programming is like building a multilingual puzzle");
+{ 
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 
